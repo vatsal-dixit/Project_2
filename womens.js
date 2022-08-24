f9e68541edc65a89f94ed7948cd46ba69ba92c79
Vatsal Dixit <vatsal1769.be20@chitkara.edu.in>
2022-08-24 21:47:13 +0530

    project 2

---
+++ womens.js
@@ -0,0 +1,21 @@
+var result = document.getElementById("content");
+
+const getItems = () => {
+    let url = "https://fakestoreapi.com/products";
+    fetch(url).then((res) => {
+        return res.json();
+    }).then((res) => {
+        result.innerHTML = "";
+        let arr = res.filter((obj) => {return obj.category == "women's clothing"});
+        console.log(arr);
+        arr.forEach(element => {
+            result.innerHTML = result.innerHTML + `<div class="item">
+            <image src=${element.image}><br>
+            <p>${element.title}</p>
+            <p class="price">$${element.price}</p>
+            <p class="rating">${element.rating.rate} <i class="fa-solid fa-star"></i> (${element.rating.count})</p>
+        </div>`;
+        })
+    })
+};
+getItems();
